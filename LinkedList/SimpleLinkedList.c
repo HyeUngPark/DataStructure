5c58655163448d1c5a388a15cd3f430e9f40e6f3
HyeUngPark <phu4860@gmail.com>
2019-03-28 01:34:39 +0900

    insert, delete function modify & findAll function adding

--- LinkedList/SimpleLinkedList.c
+++ LinkedList/SimpleLinkedList.c
@@ -1,12 +1,3 @@
-/*
-ㅇ 연결리스트
-    장점 : 재배열이 쉬움
-
-1. 단순 연결리스트
-    - 정보를 저장하는 노드와 바로 다음 노드를 가리키는 링크 하나로 구성
-    - head와 tail(null로 표현하는 경우도 있음)이 필요
-
-*/
 #include "stdio.h"
 #include "stdlib.h"
 
@@ -37,40 +28,51 @@
     }
 
     int delete_list(node *t){
-        node *s;
-        if(t->next == tail){
-            return 0;
-        }else{
-            s = t->next;
-            t->next = t->next->next;
-            free(s);
-            return 1;
+        node *s = head;
+        while(s->next != tail){
+            if(s->next==t){
+                s->next = t->next;
+                free(t);
+                break;
+            }else{
+                s = s->next;
+            } 
         }
     }
 
     node *find_node(int key){
-        node *s;
-        s=head->next;
-        while(s->key != key && s->key != tail->key){
-            s = s->next;
+        node *s = head->next;
+        while(s != tail->next){
+            if(key == s->key){
+                break;
+            }else{
+                s = s->next;
+            }
         }
         return s;
+    }
+
+    void findAll(){
+        node * t = head->next;
+        printf("#########findAll()########\n");
+        while(t != tail){
+            printf("find(%d) = %s , address = %d\n",t->key,find_node(t->key)->value,&t->next);
+            t = t->next;
+        }
+        printf("##########################\n");
     }
 
     void main(){
         init_list();
         insert_list(0,"apple",head);
-        insert_list(2,"banana",find_node(0));
-        insert_list(1,"grape",find_node(2));
+        insert_list(1,"banana",find_node(0));
+        insert_list(2,"melon",find_node(1));
+        insert_list(3,"grape",find_node(2));
+        insert_list(4,"pineapple",find_node(3));
+
+        delete_list(find_node(2));
+        delete_list(find_node(1));
         
-        printf("findAll \n");
-        node * t = head->next;
-       
-        while(t->key != tail->key){
-            printf("find(%d) = %s , address = %d\n",t->key,find_node(t->key)->value,&t->next);
-            t = t->next;
-        }
-        
-        // printf("find_node(%d)",);
+        findAll();
     }
 
